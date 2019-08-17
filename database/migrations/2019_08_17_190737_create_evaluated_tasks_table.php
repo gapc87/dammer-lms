583cdf55a1f2a312927a163bb1a0272f7d328c8e
Gabriel Perez <gapc87@gmail.com>
2019-08-17 21:13:33 +0200

    Create 2019_08_17_190737_create_evaluated_tasks_table.php

---
+++ database/migrations/2019_08_17_190737_create_evaluated_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluatedTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluated_tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('score');
+            $table->string('upload');
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('task_id');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluated_tasks');
+    }
+}
