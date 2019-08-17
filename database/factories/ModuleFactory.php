d67639899a55f1f1cb2a6d90e300a850b71acd6d
Gabriel Perez <gapc87@gmail.com>
2019-08-18 00:02:40 +0200

    Update ModuleFactory.php

--- database/factories/ModuleFactory.php
+++ database/factories/ModuleFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Module::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->jobTitle,
+        'teacher_id' => null
     ];
 });
