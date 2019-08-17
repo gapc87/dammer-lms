7178e19b0a07fd1f6ee94e0bc3bdb3ba56acccf6
Gabriel Perez <gapc87@gmail.com>
2019-08-17 19:03:21 +0200

    Update Module.php

--- app/Module.php
+++ app/Module.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'teacher_id'
+    ];
+
+    /**
+     * Un usuario es el profesor de un módulo
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function teacher()
+    {
+        return $this->belongsTo('users');
+    }
 }
