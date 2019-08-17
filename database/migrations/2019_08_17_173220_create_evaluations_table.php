f66e2340d444161ece7971e3b03a0a99b59697c3
Gabriel Perez <gapc87@gmail.com>
2019-08-17 19:33:52 +0200

    migracion y modelo Evaluation

---
+++ database/migrations/2019_08_17_173220_create_evaluations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evaluations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('evaluation');
+            $table->unsignedBigInteger('module_id');
+            $table->timestamps();
+
+            $table->foreign('module_id')->references('id')->on('modules');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evaluations');
+    }
+}
