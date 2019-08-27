98be83629647b02e3ad169f6e907d1c4fb3a3aee
Gabriel Perez <gapc87@gmail.com>
2019-08-27 19:54:53 +0200

    fontawesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,23 @@
 require('./bootstrap');
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import libraryData from './assets/fontawesome';
 import StoreData from './assets/store/index';
 import router from './assets/routes.js';
 import VueRouter from 'vue-router';
 import Vuex from 'vuex';
-
 import Vue from 'vue';
+
 Vue.use(VueRouter);
 Vue.use(Vuex);
 
 export const store = new Vuex.Store(StoreData);
 
+library.add(
+    libraryData
+);
+
+Vue.component('font-awesome-icon', require('@fortawesome/vue-fontawesome').FontAwesomeIcon);
 Vue.component('vue-layout', require('./components/Layout').default);
 
 const app = new Vue({
---
+++ resources/js/assets/fontawesome.js
@@ -0,0 +1,16 @@
+import {
+    faUserSecret,
+    faBell,
+    faEnvelope,
+    faCheckCircle,
+    faList
+} from '@fortawesome/free-solid-svg-icons';
+
+
+export default {
+    faUserSecret,
+    faBell,
+    faEnvelope,
+    faCheckCircle,
+    faList
+}
